f027f6bbec9910db231a8ef843c27b6216b0cdf6
Ellmo Rivetti <ellmorivetti@gmail.com>
2020-10-14 11:43:29 +0200

    Ajout mouvement draw line

---
+++ Assets/Scripts/PathCreator.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using UnityEngine.AI;
+using System;
+
+public class PathCreator : MonoBehaviour
+{
+    public static Vector3[] path = new Vector3[0];
+
+    LineRenderer lr;
+
+    private List<Vector3> travel_points = new List<Vector3>();
+    public Action<IEnumerable<Vector3>> OnNewPathCreated = delegate { };
+
+    void Start()
+    {
+        lr = GetComponent<LineRenderer>();
+
+        //lr.SetPosition(0, gameObject.transform.position);
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            travel_points.Clear();
+        }
+
+        if(Input.GetMouseButton(0))
+        {
+            RaycastHit hit;
+
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
+            {
+                if(DistanceToLastPoint(hit.point) > 1f)
+                {
+                    travel_points.Add(hit.point);
+                    lr.positionCount = travel_points.Count;
+                    lr.SetPositions(travel_points.ToArray());
+                }
+            }
+        }
+        else if(Input.GetMouseButtonUp(0))
+        {
+            OnNewPathCreated(travel_points);
+        }
+    }
+
+    float DistanceToLastPoint(Vector3 point)
+    {
+        if(!travel_points.Any())
+        {
+            return Mathf.Infinity;
+        }
+        return Vector3.Distance(travel_points.Last(), point);
+    }
+}
---
+++ Assets/Scripts/PathMover.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using UnityEngine.AI;
+using System;
+
+public class PathMover : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    private Queue<Vector3> pathPoints = new Queue<Vector3>();
+
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        FindObjectOfType<PathCreator>().OnNewPathCreated += SetPoints;
+    }
+
+    private void SetPoints(IEnumerable<Vector3> points)
+    {
+        pathPoints = new Queue<Vector3>(points);
+    }
+
+    private void Update()
+    {
+        if (ShouldSetDestination())
+            agent.SetDestination(pathPoints.Dequeue());
+    }
+
+    private bool ShouldSetDestination()
+    {
+        if (pathPoints.Count == 0)
+            return false;
+
+        if (agent.hasPath == false || agent.remainingDistance < 0.5f)
+            return true;
+
+        return false;
+    }
+}
