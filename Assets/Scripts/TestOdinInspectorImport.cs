b782cd1ac188580616582f53a33ed46be42ba059
Stanislaw PLUSZKIEWICZ <stanislaw.karol.pluszkiewicz@gmail.com>
2020-11-21 16:39:43 +0100

    Import Odin Inspector

---
+++ Assets/Scripts/TestOdinInspectorImport.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+public class TestOdinInspectorImport : MonoBehaviour
+{
+	[Button]
+    public void TestImport(string message)
+	{
+		print(message);
+	}
+}
