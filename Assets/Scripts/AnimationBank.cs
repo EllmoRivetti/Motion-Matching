152f61e3b664625c6318e7a4301a8e7e5a00080e
Stanislaw PLUSZKIEWICZ <stanislaw.karol.pluszkiewicz@gmail.com>
2020-11-21 16:34:16 +0100

    AnimationBank

---
+++ Assets/Scripts/AnimationBank.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "AnimationBank", menuName = "MotionMaching/AnimationBank")]
+public class AnimationBank : ScriptableObject
+{
+	public List<AnimationClip> m_Animations;
+}
