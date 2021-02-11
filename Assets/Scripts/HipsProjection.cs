12bab687d15ca348e95e764f7884d5aa0ba1707b
Gillian THOMAS <36472202+Wooneg@users.noreply.github.com>
2021-02-11 12:18:12 +0100

    Update HipsProjection + scene

--- Assets/Scripts/HipsProjection.cs
+++ Assets/Scripts/HipsProjection.cs
@@ -5,44 +5,56 @@ using UnityEngine;
 public class HipsProjection : MonoBehaviour
 {
 
-    [SerializeField] GameObject Hips, RFeet, LFeet;
-    private GameObject Repere, CurrentPlatform, Ground;
+    [SerializeField] GameObject m_Hips, m_RFeet, m_LFeet, m_Hips2DOnGround;
+    private GameObject m_CurrentPlatform, m_Ground;
 
     public bool m_DrawArrow = true;
 
-    private Vector3 _hipsGroundPosition { get; set; }
-    private Vector3 _rFeetHipsPosition { get; set; }
-    private Vector3 _lFeetHipsPosition { get; set; }
+    private Vector3 m_hipsGroundPosition { get; set; }
+    private Vector3 m_rFeetHipsPosition { get; set; }
+    private Vector3 m_lFeetHipsPosition { get; set; }
+
+    public Vector3 getHips2DOnGroundPosition()
+    {
+        return m_Hips2DOnGround.transform.position;
+    }
+
+    public Vector3 getHips2DOnGroundRotation()
+    {
+        return m_Hips2DOnGround.transform.forward;
+    }
+
 
     void Start()
     {
-        Repere = new GameObject();
-        Repere.name = "HipsRepere";
     }
 
     void Update()
     {
         if (!m_DrawArrow)
             return;
-        Vector3 HipsPosition = Hips.transform.position;
-        Vector3 HipsDirection = Hips.transform.forward;
+        Vector3 hipsPosition = m_Hips.transform.position;
+        Vector3 hipsDirection = m_Hips.transform.forward;
 
-        Vector3 RFeetPosition = RFeet.transform.position;
-        Vector3 RFeetDirection = RFeet.transform.forward;
+        Vector3 rFeetPosition = m_RFeet.transform.position;
+        Vector3 rFeetDirection = m_RFeet.transform.forward;
 
-        Vector3 LFeetPosition = LFeet.transform.position;
-        Vector3 LFeetDirection = LFeet.transform.forward;
+        Vector3 lFeetPosition = m_LFeet.transform.position;
+        Vector3 lFeetDirection = m_LFeet.transform.forward;
 
-        if (Ground != null)
+        if (m_Ground != null)
         {
-            _hipsGroundPosition = Ground.transform.InverseTransformPoint(Hips.transform.position);
-            Repere.transform.position = _hipsGroundPosition;
-            _rFeetHipsPosition = Repere.transform.InverseTransformPoint(RFeet.transform.position);
-            _lFeetHipsPosition = Repere.transform.InverseTransformPoint(LFeet.transform.position);
+            Vector3 hipsPosition2D = new Vector3(hipsPosition.x, 0, hipsPosition.z);
+
+            m_Hips2DOnGround.transform.position = hipsPosition2D;
+            m_Hips2DOnGround.transform.LookAt(m_Hips2DOnGround.transform.position + m_Hips.transform.forward.normalized, Vector3.up);
+
+            m_rFeetHipsPosition = m_Hips2DOnGround.transform.InverseTransformPoint(m_RFeet.transform.position);
+            m_lFeetHipsPosition = m_Hips2DOnGround.transform.InverseTransformPoint(m_LFeet.transform.position);
         
-            drawArrowOnGround(HipsPosition, HipsDirection, Color.red);
-            drawArrowOnGround(RFeetPosition, RFeetDirection, Color.blue);
-            drawArrowOnGround(LFeetPosition, LFeetDirection, Color.green);
+            drawArrowOnGround(m_Hips2DOnGround.transform.position, m_Hips2DOnGround.transform.forward, Color.red);
+            drawArrowOnGround(rFeetPosition, rFeetDirection, Color.blue);
+            drawArrowOnGround(lFeetPosition, lFeetDirection, Color.green);
         }
     }
 
@@ -52,14 +64,14 @@ public class HipsProjection : MonoBehaviour
         if(collision.gameObject.tag == "Platform")
         {
             // Debug.Log("Platform change");
-            CurrentPlatform = collision.gameObject;
-            Ground = collision.gameObject.transform.parent.gameObject;
+            m_CurrentPlatform = collision.gameObject;
+            m_Ground = collision.gameObject.transform.parent.gameObject;
         }
     }
     void drawArrowOnGround(Vector3 position,Vector3 direction, Color color)
     {
-        direction.y = CurrentPlatform.transform.rotation.y;
-        position.y = CurrentPlatform.transform.position.y + 0.1f;
+        direction.y = m_CurrentPlatform.transform.rotation.y;
+        position.y = m_CurrentPlatform.transform.position.y + 0.1f;
         float arrowHeadLength = 0.25f, arrowHeadAngle = 20.0f;
         
         Vector3 right = Quaternion.LookRotation(direction) * Quaternion.Euler(0, 180 + arrowHeadAngle, 0) * new Vector3(0, 0, 1);
