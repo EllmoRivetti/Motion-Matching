18dc84b1e0fa6924c5bcff91da6b3f767e8e95f3
Gillian THOMAS <36472202+Wooneg@users.noreply.github.com>
2021-02-21 15:19:58 +0100

    Retirer la condition du sol de HipsProjection

--- Assets/Scripts/HipsProjection.cs
+++ Assets/Scripts/HipsProjection.cs
@@ -4,7 +4,7 @@ public class HipsProjection : MonoBehaviour
 {
 
     [SerializeField] GameObject m_Hips, m_RFeet, m_LFeet, m_Hips2DOnGround;
-    private GameObject m_CurrentPlatform, m_Ground;
+    private GameObject m_CurrentPlatform
 
     public bool m_DrawArrow = true;
 
@@ -40,32 +40,21 @@ public class HipsProjection : MonoBehaviour
         Vector3 lFeetPosition = m_LFeet.transform.position;
         Vector3 lFeetDirection = m_LFeet.transform.forward;
 
-        if (m_Ground != null)
-        {
-            Vector3 hipsPosition2D = new Vector3(hipsPosition.x, 0, hipsPosition.z);
+        Vector3 hipsPosition2D = new Vector3(hipsPosition.x, 0, hipsPosition.z);
 
-            m_Hips2DOnGround.transform.position = hipsPosition2D;
-            m_Hips2DOnGround.transform.LookAt(m_Hips2DOnGround.transform.position + m_Hips.transform.forward.normalized, Vector3.up);
+        m_Hips2DOnGround.transform.position = hipsPosition2D;
+        m_Hips2DOnGround.transform.LookAt(m_Hips2DOnGround.transform.position + m_Hips.transform.forward.normalized, Vector3.up);
 
-            m_rFeetHipsPosition = m_Hips2DOnGround.transform.InverseTransformPoint(m_RFeet.transform.position);
-            m_lFeetHipsPosition = m_Hips2DOnGround.transform.InverseTransformPoint(m_LFeet.transform.position);
+        m_rFeetHipsPosition = m_Hips2DOnGround.transform.InverseTransformPoint(m_RFeet.transform.position);
+        m_lFeetHipsPosition = m_Hips2DOnGround.transform.InverseTransformPoint(m_LFeet.transform.position);
         
-            drawArrowOnGround(m_Hips2DOnGround.transform.position, m_Hips2DOnGround.transform.forward, Color.red);
-            drawArrowOnGround(rFeetPosition, rFeetDirection, Color.blue);
-            drawArrowOnGround(lFeetPosition, lFeetDirection, Color.green);
-        }
+        drawArrowOnGround(m_Hips2DOnGround.transform.position, m_Hips2DOnGround.transform.forward, Color.red);
+        drawArrowOnGround(rFeetPosition, rFeetDirection, Color.blue);
+        drawArrowOnGround(lFeetPosition, lFeetDirection, Color.green);
+        
     }
 
     #region draw arrow on ground
-    private void OnCollisionEnter(Collision collision)
-    {
-        if(collision.gameObject.tag == "Ground")
-        {
-            // Debug.Log("Platform change");
-            m_CurrentPlatform = collision.gameObject;
-            m_Ground = collision.gameObject.transform.parent.gameObject;
-        }
-    }
     void drawArrowOnGround(Vector3 position,Vector3 direction, Color color)
     {
         direction.y = m_CurrentPlatform.transform.rotation.y;
